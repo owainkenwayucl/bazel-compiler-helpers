c4811f4fc7c9044b4f3f6fc252f59dd838a6253a
Owain Kenway <o.kenway@ucl.ac.uk>
2018-06-14 12:12:44 +0100

    Fix path.

--- bazel-intel-wrapper.sh
+++ bazel-intel-wrapper.sh
@@ -5,6 +5,6 @@
 
 export INTEL_LICENSE_FILE=28518@lic-intel.ucl.ac.uk
 
-command=`basename $0`
+command=/shared/ucl/apps/intel/2018.Update3/compilers_and_libraries_2018.3.222/linux/bin/intel64/`basename $0`
 
-$0 $@
+$command $@
